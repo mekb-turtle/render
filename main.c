a33bde87b0b130a98e20b2de0cd1be07412b4d59
mekb the turtle <mekbgithub@icloud.com>
2022-07-10 21:48:17 +1000

    working

--- main.c
+++ main.c
@@ -9,20 +9,25 @@
 #include <string.h>
 #include <errno.h>
 #define BLOCK 1024
+struct Triangle {
+	float x1, y1, z1, x2, y2, z2, x3, y3, z3;
+	float r, g, b, a;
+};
+size_t triangle_len = 0;
+struct Triangle *triangles = NULL;
 void display() {
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 	glPointSize(1.0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glEnable(GL_DEPTH_TEST);
-	/*
-	glColor3f(0.0, 0.0, 0.0);
-	glBegin(GL_POLYGON);
-	glVertex3f(+0.5, -0.5, -0.5);
-	glVertex3f(+0.5, +0.5, -0.5);
-	glVertex3f(-0.5, +0.5, -0.5);
-	glVertex3f(-0.5, -0.5, -0.5);
-	glEnd();
-	*/
+	for (size_t i = 0; i <= triangle_len; ++i) {
+		glColor4f(triangles[i].r, triangles[i].g, triangles[i].b, triangles[i].a);
+		glBegin(GL_POLYGON);
+		glVertex3f(triangles[i].x1, triangles[i].y1, triangles[i].z1);
+		glVertex3f(triangles[i].x2, triangles[i].y2, triangles[i].z2);
+		glVertex3f(triangles[i].x3, triangles[i].y3, triangles[i].z3);
+		glEnd();
+	}
 	glFlush();
 }
 void special() {
@@ -34,10 +39,6 @@ Usage: %s <file>\n\
 ", argv0);
 	return 2;
 }
-struct Triangle {
-	float x1, y1, z1, x2, y2, z2, x3, y3, z3;
-	float r, g, b, a;
-};
 int main(int argc, char* argv[]) {
 #define INVALID { return usage(argv[0]); }
 	char *file_name = NULL;
@@ -62,15 +63,13 @@ int main(int argc, char* argv[]) {
 		fprintf(stderr, "%s: %s\n", file_name, strerror(errno));
 		return errno;
 	}
-	size_t i = 0;
 	size_t size = 0;
-	struct Triangle *triangles = NULL;
 	while (1) {
 		struct Triangle tri;
 		unsigned long col;
 		if (ferror(file)) { fprintf(stderr, "Error reading\n"); break; }
 		if (feof(file)) break;
-		fscanf(file, "%le %le %le  %le %le %le  %le %le %le  %lx\n",
+		fscanf(file, "%e %e %e  %e %e %e  %e %e %e  %lx\n",
 			&tri.x1, &tri.y1, &tri.z1, &tri.x2, &tri.y2, &tri.z2, &tri.x3, &tri.y3, &tri.z3, &col);
 		col &= 0xffffffff;
 		if (col <= 0xffffff) {
@@ -81,16 +80,16 @@ int main(int argc, char* argv[]) {
 		tri.g = (float)((col & 0x00ff0000) >> 020) / 0xff;
 		tri.b = (float)((col & 0x0000ff00) >> 010) / 0xff;
 		tri.a = (float)((col & 0x000000ff) >> 000) / 0xff;
-		if (i >= size) {
+		if (triangle_len >= size) {
 			size += BLOCK;
 			triangles = realloc(triangles, size * sizeof(struct Triangle));
 		}
-		triangles[i] = tri;
-		if (i == SIZE_MAX || i == 0xffffff) {
+		triangles[triangle_len] = tri;
+		if (triangle_len == SIZE_MAX || triangle_len == 0xffffff) {
 			fprintf(stderr, "SIZE_MAX limit was hit, triangles after this will not be used\n");
 			break;
 		}
-		++i;
+		++triangle_len;
 	}
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
